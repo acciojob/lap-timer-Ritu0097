f9883af7d136f0ae2e940695ca623da965a629d9
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-10 14:33:28 +0530

    timer

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ const App = () => {
     const centiseconds = time % 100;
     return `${pad(minutes)}:${pad(seconds)}:${pad(centiseconds)}`;
   };
-
+// main logic
   return (
     <div>
       <div>{formatTime(time)}</div>
